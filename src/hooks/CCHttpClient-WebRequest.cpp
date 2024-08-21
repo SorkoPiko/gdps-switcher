bb2595ed3f332b83946c46df2f59b33d11adaa28
km7 dev <100500023+Kingminer7@users.noreply.github.com>
2024-08-21 13:39:27 -0400

    clean

--- src/hooks/CCHttpClient-WebRequest.cpp
+++ src/hooks/CCHttpClient-WebRequest.cpp
@@ -2,49 +2,16 @@
 
 using namespace geode::prelude;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 web::WebTask webSend(web::WebRequest *self, std::string_view method, std::string_view givenUrl)
-    {
-
-
-
-
-
-
-
-
-
-
-
-
-
-		return self->send(method, givenUrl);
-    };
+{
+	return self->send(method, givenUrl);
+};
 
 $execute
 {
     auto hook = Mod::get()->hook(
         reinterpret_cast<void*>(
-			geode::addresser::getNonVirtual(&web::WebRequest::send)
+        	geode::addresser::getNonVirtual(&web::WebRequest::send)
         ),
         &webSend,
         "web::WebRequest::send",
